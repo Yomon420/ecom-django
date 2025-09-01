9daa926ba0b0d4a398a201cc1472c1b6742c6b26
yomon420 <bulletx000@gmail.com>
2025-09-02 00:14:54 +0300

    fixing cors

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -39,12 +39,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",  # allow CORS
+    "corsheaders.middleware.CorsMiddleware",  
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
 ]
 
 ROOT_URLCONF = "ecommerce_backend.urls"
