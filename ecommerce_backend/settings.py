e892f2a2248814835477ddedb02cd484ca8abd86
yomon420 <bulletx000@gmail.com>
2025-09-02 16:18:12 +0300

    added swagger docs authorization token 3 (fixed static related errors)

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",  
     "django.middleware.common.CommonMiddleware",
@@ -109,6 +110,7 @@ USE_TZ = True
 # STATIC & MEDIA
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
